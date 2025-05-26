c16d022d897a7fdd3f350d53719017c15f1f9c0b
RyPaert <158561974+RyPaert@users.noreply.github.com>
2025-05-26 14:39:40 +0300

    Part 10 19:58

--- NetflixCloneMAUI/Pages/DetailsPage.xaml.cs
+++ NetflixCloneMAUI/Pages/DetailsPage.xaml.cs
@@ -7,16 +7,26 @@ public partial class DetailsPage : ContentPage
     private readonly DetailsViewModel _viewModel;
 
     public DetailsPage(DetailsViewModel viewModel)
-	{
-		InitializeComponent();
-		_viewModel = viewModel;
-		BindingContext = _viewModel;
-	}
+    {
+        InitializeComponent();
+        _viewModel = viewModel;
+        BindingContext = _viewModel;
+    }
 
     protected async override void OnAppearing()
     {
         base.OnAppearing();
         await _viewModel.InitializeAsync();
+
+    }
+
+    private void SimilarTab_Tapped(object sender, TappedEventArgs e)
+    {
+
+    }
+
+    private void TrailersTab_Tapped(object sender, TappedEventArgs e)
+    {
 
     }
 }
